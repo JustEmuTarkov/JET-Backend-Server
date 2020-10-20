c3c556baddae83e01eb40a0f33543ed80859ff89
TheMaoci <themaoci@gmail.com>
2020-10-20 12:52:45 +0200

    Mods supported + some clean up

---
+++ user/mods/TheMaoci-AllItemsExamined-1.0.0/src/callbacks/cache/allitemsexamined.js
@@ -0,0 +1,17 @@
+exports.cache = () => {
+    if (!serverConfig.rebuildCache) {
+        return;
+    }
+
+    logger.logInfo("Caching: [MOD] AllItemsExamined");
+
+    let base = json.parse(json.read(db.user.cache.items));
+
+    for (let file in base.data) {
+        let data = base.data[file];
+        data._props.ExaminedByDefault = true;
+        base.data[file] = data;
+    }
+
+    json.write(db.user.cache.items, base);
+}
