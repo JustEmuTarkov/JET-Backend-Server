5c8b6b444558e0ef2ea6b7b8e10f44205227d391
TheMaoci <themaoci@gmail.com>
2020-10-27 15:06:50 +0100

    kinda fixed no profile found if new account

--- src/response/client.game.start.js
+++ src/response/client.game.start.js
@@ -1,6 +1,12 @@
 exports.execute = (url, info, sessionID) => {
-	if(json.exist("user/profiles/" + sessionID + "/character.json"))
-		return response_f.getBody(null, 0, null);
-		
+	let accounts = json.readParsed(db.user.configs.accounts);
+	for(let account in accounts){
+		if(account == sessionID)
+		{
+			if(!json.exist("user/profiles/" + sessionID + "/character.json"))
+				logger.logWarning("New account login!");
+			return response_f.getBody(null, 0, null);
+		}
+	}
 	return response_f.getBody(null, 999, "Profile Not Found!!");
 }
