d8ac801201842163e3a46183236879c6420806cb
TheMaoci <themaoci@gmail.com>
2020-11-30 14:03:50 +0100

    Small Missed Fix ... yea...

--- src/response/dynamic/singleplayer.settings.bot.difficulty.js
+++ src/response/dynamic/singleplayer.settings.bot.difficulty.js
@@ -2,5 +2,5 @@ exports.execute = (url, info, sessionID) => {
 	const splittedUrl = url.split("/");
 	const type = splittedUrl[splittedUrl.length - 2].toLowerCase();
 	const difficulty = splittedUrl[splittedUrl.length - 1];
-	return response_f.getBody(bots_f.getBotDifficulty(type, difficulty));
+	return response_f.noBody(bots_f.getBotDifficulty(type, difficulty));
 }
--- src/response/dynamic/singleplayer.settings.bot.limit.js
+++ src/response/dynamic/singleplayer.settings.bot.limit.js
@@ -1,5 +1,5 @@
 exports.execute = (url, info, sessionID) => {
 	const splittedUrl = url.split("/");
 	const type = splittedUrl[splittedUrl.length - 1];
-	return response_f.getBody(bots_f.getBotLimit(type));	
+	return response_f.noBody(bots_f.getBotLimit(type));	
 }
