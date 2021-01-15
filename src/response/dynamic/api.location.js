792b57ecd12e1e10a655388969a11ccb3ed09973
kiobu <cdolson30@gmail.com>
2021-01-15 13:50:57 -0500

    Update bots db with correct dirnames. Actually return a response in api.location.js

--- src/response/dynamic/api.location.js
+++ src/response/dynamic/api.location.js
@@ -1,6 +1,4 @@
 exports.execute = (url, info, sessionID) => {
-	if(url.includes("factory4_day"))
-		response_f.noBody(fileIO.readParsed(db.locations_test.factory4_day1).Location);
-	
-	response_f.noBody(location_f.handler.get(url.replace("/api/location/", "")))
+	// if (url.includes("factory4_day")) { return response_f.noBody(fileIO.readParsed(db.locations_test.factory4_day1).Location); }
+	return response_f.noBody(location_f.handler.get(url.replace("/api/location/", "")))
 };
