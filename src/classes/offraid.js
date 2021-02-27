092b6f13396a53ad82be24dba1ad556117159111
kiobu <cdolson30@gmail.com>
2021-02-27 13:44:16 -0500

    Forgot to remove console.log again :)

--- src/classes/offraid.js
+++ src/classes/offraid.js
@@ -334,8 +334,7 @@ function saveProgress(offraidData, sessionID) {
     //offraidData.profile.Inventory.items = helper_f.replaceIDs(offraidData.profile, offraidData.profile.Inventory.items, offraidData.profile.Inventory.fastPanel);
 
     // If an item ID or parent ID is over 24 characters (the max), shorten it. This should fix
-    // a bug where you can't sell rounds unloaded from a magazine mid-raid. -- kiobu 
-    console.log(offraidData.profile.Inventory)
+    // a bug where you can't sell rounds unloaded from a magazine mid-raid. -- kiobu
     for (let k in offraidData.profile.Inventory.items) {
         let item = offraidData.profile.Inventory.items[k]
         if (item._id && item._id.length > 24) {
