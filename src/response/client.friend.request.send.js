2cb1aea43647e0de751540a66e22970ef197165a
Cheese <yolosolodbaz@gmail.com>
2021-03-17 07:30:50 -0600

    Added search functionality in friends list & handled sending friend request by returning empty response

---
+++ src/response/client.friend.request.send.js
@@ -0,0 +1,2 @@
+// Not implemented
+exports.execute = (url, info, sessionID) => response_f.noBody({requestId: "¯\_(ツ)_/¯", retryAfter: 0, status: 0});
--- src/response/client.game.profile.search.js
+++ src/response/client.game.profile.search.js
@@ -1,22 +1,21 @@
 exports.execute = (url, info, sessionID) => {
-	//let ids = fileIO.readDir("user/profiles").filter(x => x != sessionID && x.startsWith("AID"));
-	//let users = [];
-	//for(let i in ids){
-	//	let id = ids[i];
-	//	if(!fileIO.exist(`user/profiles/${id}/character.json`)) continue;
-	//	let character = fileIO.readParsed(`user/profiles/${id}/character.json`);
-	//	if(!character.Info.Nickname || !character.Info.Nickname.toLowerCase().includes(info.nickname.toLowerCase())) continue;
-	//	let obj = {_info: {}};
-	//	obj._id = character.aid;
-	//	obj._info.Nickname = character.Info.Nickname;
-	//	obj._info.Side = character.Info.Side;
-	//	obj._info.Level = character.Info.Level;
-	//	obj._info.MemberCategory = ["Default"];
-	//	obj._info.ignored = false;
-	//	obj._info.banned = false;
-	//	users.push(obj);
-	//}
-	//console.log(users);
-	
-	return response_f.getBody([]);
+	let ids = Object.keys(account_f.handler.accounts).filter(x => x != sessionID);
+	let users = [];
+	for(let i in ids){
+		let id = ids[i];
+		if(!fileIO.exist(`user/profiles/${id}/character.json`)) continue;
+		let character = fileIO.readParsed(`user/profiles/${id}/character.json`);
+		if(!character.Info.Nickname || !character.Info.Nickname.toLowerCase().includes(info.nickname.toLowerCase())) continue;
+		let obj = {Info: {}};
+		obj._id = character.aid;
+		obj.Info.Nickname = character.Info.Nickname;
+		obj.Info.Side = character.Info.Side;
+		obj.Info.Level = character.Info.Level;
+		obj.Info.MemberCategory = character.Info.MemberCategory;
+		obj.Info.Ignored = false;
+		obj.Info.Banned = false;
+		users.push(obj);
+	}
+
+	return response_f.getBody(users);
 }
