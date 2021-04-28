079e6d1dde99d6ba8b4d849d04b646c177f7ef76
TheMaoci <themaoci@gmail.com>
2021-04-28 17:40:16 +0200

    items response not from cache but from server memory

--- src/response/client.items.js
+++ src/response/client.items.js
@@ -1,6 +1,3 @@
 exports.execute = (url, info, sessionID) => {
-	global._database.items = require("../../" + db.user.cache.items);
-	if(typeof global._database.items.data != "undefined")
-		global._database.items = global._database.items.data;
 	return response_f.getBody(global._database.items);
 }
