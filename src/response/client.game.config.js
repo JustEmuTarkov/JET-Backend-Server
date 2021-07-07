148a871776dacd6631ce22017b9c623ae65911cb
kiobu <cdolson30@gmail.com>
2021-07-07 12:53:26 -0400

    Fix client config.

--- src/response/client.game.config.js
+++ src/response/client.game.config.js
@@ -1,35 +1,4 @@
 exports.execute = (url, info, sessionID) => {
-<<<<<<< HEAD
-	return response_f.getBody(
-		{
-			"queued": false, 
-			"banTime": 0, 
-			"hash": "BAN0", 
-			"lang": "en", 
-			"languages": {
-				"en": "English",
-				"fr": "French",
-				"ge": "German",
-				"ru": "Русский"
-			},
-			"ndaFree": false,
-			"reportAvailable": true,
-			"aid": sessionID, 
-			"token": sessionID, 
-			"taxonomy": 6, 
-			"activeProfileId": "pmc" + sessionID, 
-			"nickname": "user", 
-			"backend": {
-				"Trading": server.getBackendUrl(), 
-				"Messaging": server.getBackendUrl(), 
-				"Main": server.getBackendUrl(), 
-				"RagFair": server.getBackendUrl()
-			}, 
-			"totalInGame": 0,
-			"utc_time": utility.getTimestamp()
-		}
-	);
-=======
 	var obj = 
 	{
 		"queued": false, 
@@ -59,5 +28,4 @@ exports.execute = (url, info, sessionID) => {
 		obj.languages[lang.ShortName] = lang.Name;
 	}
 	return response_f.getBody(obj);
->>>>>>> 9aff2d7c0000b3fc623757ffdea4c2cd18db915b
 }
