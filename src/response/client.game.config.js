ab91203c718a0d016e0325d876531e551b483af6
Cheese <yolosolodbaz@gmail.com>
2021-07-07 10:07:42 -0600

    Added dynamic locales and fixed the error occurring on the nickname property.

--- src/response/client.game.config.js
+++ src/response/client.game.config.js
@@ -1,31 +1,32 @@
 exports.execute = (url, info, sessionID) => {
-	return response_f.getBody(
-		{
-			"queued": false, 
-			"banTime": 0, 
-			"hash": "BAN0", 
-			"lang": "en", 
-			"languages": {
-				"en": "English",
-				"fr": "French",
-				"ge": "German",
-				"ru": "Русский"
-			},
-			"ndaFree": false,
-			"reportAvailable": true,
-			"aid": sessionID, 
-			"token": sessionID, 
-			"taxonomy": 6, 
-			"activeProfileId": "pmc" + sessionID, 
-			"nickname": profile_f.handler.getPmcProfile(sessionID).Info.Nickname, 
-			"backend": {
-				"Trading": server.getBackendUrl(), 
-				"Messaging": server.getBackendUrl(), 
-				"Main": server.getBackendUrl(), 
-				"RagFair": server.getBackendUrl()
-			}, 
-			"totalInGame": 0,
-			"utc_time": utility.getTimestamp()
-		}
-	);
+	var obj = 
+	{
+		"queued": false, 
+		"banTime": 0, 
+		"hash": "BAN0", 
+		"lang": "en", 
+		"ndaFree": false,
+		"reportAvailable": true,
+		"languages": {},
+		"aid": sessionID, 
+		"token": sessionID, 
+		"taxonomy": 6, 
+		"activeProfileId": "pmc" + sessionID, 
+		"nickname": "user", 
+		"backend": {
+			"Trading": server.getBackendUrl(), 
+			"Messaging": server.getBackendUrl(), 
+			"Main": server.getBackendUrl(), 
+			"RagFair": server.getBackendUrl()
+		}, 
+		"totalInGame": 0,
+		"utc_time": utility.getTimestamp()
+	}
+	let languages = locale_f.handler.getLanguages().data;
+	console.log(languages);
+	for(let index in languages){
+		var lang = languages[index];
+		obj.languages[lang.ShortName] = lang.Name;
+	}
+	return response_f.getBody(obj);
 }
