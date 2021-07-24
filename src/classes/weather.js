8a6f45e17f4045f02fe8885fc64e4dab26e628e8
TheMaoci <themaoci@gmail.com>
2021-07-24 19:04:09 +0200

    weather fix + server order of loading changed (devs important news)

--- src/classes/weather.js
+++ src/classes/weather.js
@@ -2,43 +2,38 @@
 
 let weather = undefined;
 
-function initialize() {
-    weather = fileIO.readParsed(db.user.cache.weather);
-}
-
 function generate() {
-    let output = {};
+  let output = {};
 
-    // set weather
-    if (global._database.gameplayConfig.location.forceWeatherEnabled) {
-        output = weather.data[global._database.gameplayConfig.location.forceWeatherId];
-    } else {
-        output = weather.data[utility.getRandomInt(0, weather.data.length - 1)];
-    }
+  // set weather
+  if (global._database.gameplayConfig.location.forceWeatherEnabled) {
+    output = global._database.weather.data[global._database.gameplayConfig.location.forceWeatherId];
+  } else {
+    output = global._database.weather.data[utility.getRandomInt(0, global._database.weather.data.length - 1)];
+  }
 
-    // replace date and time
-    if (global._database.gameplayConfig.location.realTimeEnabled) {
-        // Apply acceleration to time computation.
-        let timeInSeconds = new Date().getTime() / 1000; // date in seconds
-        let deltaSeconds = utility.getServerUptimeInSeconds() * output.acceleration;
-		
-		let newDateInSeconds = timeInSeconds + deltaSeconds; 
-		
-		let newDateObj = new Date(newDateInSeconds * 1000);
+  // replace date and time
+  if (global._database.gameplayConfig.location.realTimeEnabled) {
+    // Apply acceleration to time computation.
+    let timeInSeconds = new Date().getTime() / 1000; // date in seconds
+    let deltaSeconds = utility.getServerUptimeInSeconds() * output.acceleration;
 
-        let time = utility.formatTime(newDateObj).replace("-", ":").replace("-", ":");
-        let date = utility.formatDate(newDateObj);
-        let datetime = `${date} ${time}`;
+    let newDateInSeconds = timeInSeconds + deltaSeconds;
 
-        output.weather.timestamp = Math.floor(newDateObj / 1000);
-        output.weather.date = date;
-        output.weather.time = datetime;
-        output.date = date;
-        output.time = time;
-    }
+    let newDateObj = new Date(newDateInSeconds * 1000);
 
-    return output;
+    let time = utility.formatTime(newDateObj).replace("-", ":").replace("-", ":");
+    let date = utility.formatDate(newDateObj);
+    let datetime = `${date} ${time}`;
+
+    output.weather.timestamp = Math.floor(newDateObj / 1000);
+    output.weather.date = date;
+    output.weather.time = datetime;
+    output.date = date;
+    output.time = time;
+  }
+
+  return output;
 }
 
-module.exports.initialize = initialize;
 module.exports.generate = generate;
