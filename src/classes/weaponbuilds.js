849b606116c0f2c99d4cbc2ed7fb41ba7ecfc929
TheMaoci <themaoci@gmail.com>
2021-07-31 22:50:25 +0200

    fix weapon build updating in client (not after softRestart)

--- src/classes/weaponbuilds.js
+++ src/classes/weaponbuilds.js
@@ -1,54 +1,54 @@
 "use strict";
 
 function getPath(sessionID) {
-	let path = db.user.profiles.userbuilds;
-	return path.replace("__REPLACEME__", sessionID);
+  return `user/profiles/${sessionID}/userbuilds.json`;
 }
 
 function getUserBuilds(sessionID) {
-	let userBuildsMap = fileIO.readParsed(getPath(sessionID));
+  let userBuildsMap = fileIO.readParsed(getPath(sessionID));
 
-	let userBuilds = [];
+  let userBuilds = [];
 
-	for (let buildName in userBuildsMap) {
-		userBuilds.push(userBuildsMap[buildName]);
-	}
+  for (let buildName in userBuildsMap) {
+    userBuilds.push(userBuildsMap[buildName]);
+  }
 
-	return userBuilds;
+  return userBuilds;
 }
 
 function SaveBuild(pmcData, body, sessionID) {
-	delete body.Action;
-	body.id = utility.generateNewItemId();	
+  delete body.Action;
+  body.id = utility.generateNewItemId();
 
-	let output = item_f.handler.getOutput(sessionID);
-	let savedBuilds = fileIO.readParsed(getPath(sessionID));
+  let savedBuilds = fileIO.readParsed(getPath(sessionID));
 
-	// replace duplicate ID's. The first item is the base item.
-	// The root ID and the base item ID need to match.
-	body.items = helper_f.replaceIDs(pmcData, body.items, false);
-	body.root = body.items[0]._id;
+  // replace duplicate ID's. The first item is the base item.
+  // The root ID and the base item ID need to match.
+  body.items = helper_f.replaceIDs(pmcData, body.items, false);
+  body.root = body.items[0]._id;
 
-	savedBuilds[body.name] = body;
-	fileIO.write(getPath(sessionID), savedBuilds);
-	if(!output.builds)
-		output.builds = [];
-	output.builds.push(body);
-    return output;
+  let output = item_f.handler.getOutput(sessionID);
+  output.profileChanges[pmcData._id].builds.push(body);
+  item_f.handler.setOutput(output);
+
+  savedBuilds[body.name] = body;
+  fileIO.write(getPath(sessionID), savedBuilds);
+
+  return output;
 }
 
 function RemoveBuild(pmcData, body, sessionID) {
-    let savedBuilds = fileIO.readParsed(getPath(sessionID));
+  let savedBuilds = fileIO.readParsed(getPath(sessionID));
 
-    for (let name in savedBuilds) {
-        if (savedBuilds[name].id === body.id) {
-            delete savedBuilds[name];
-            fileIO.write(getPath(sessionID), savedBuilds);
-            break;
-        }
+  for (let name in savedBuilds) {
+    if (savedBuilds[name].id === body.id) {
+      delete savedBuilds[name];
+      fileIO.write(getPath(sessionID), savedBuilds);
+      break;
     }
+  }
 
-    return item_f.handler.getOutput(sessionID);
+  return item_f.handler.getOutput(sessionID);
 }
 
 module.exports.getPath = getPath;
