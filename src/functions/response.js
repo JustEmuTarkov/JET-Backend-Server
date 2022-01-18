7b4d09b1e51db237084dd3ada4e8f6530f2f510e
TheMaoci <themaoci@gmail.com>
2022-01-18 02:49:32 +0100

    Fix for 16440 - BSG started to use retarded check there

--- src/functions/response.js
+++ src/functions/response.js
@@ -399,7 +399,7 @@ class Responses {
     for (let account in accounts) {
       if (account == sessionID) {
         if (!fileIO.exist("user/profiles/" + sessionID + "/character.json")) logger.logWarning("New account login!");
-        return response_f.getBody(null, 0, null);
+        return response_f.getBody({utc_time: Date.now() / 1000}, 0, null);
       }
     }
     return response_f.getBody(null, 999, "Profile Not Found!!");
